eb3f15d6cad2007a1c7257c115abeb06a5bad206
Aquib Alam <aquibalam399@gmail.com>
2023-10-09 00:10:05 +0530

    scroll

--- src/components/ScheduleDetailLayout.jsx
+++ src/components/ScheduleDetailLayout.jsx
@@ -46,14 +46,14 @@ export default function ScheduleDetailLayout({ _id, title, description, time, ty
   hover:scale-105 active:text-white active:scale-100 active:bg-[rgb(70,70,70)] transition-all`;
 
   return (
-    <div className="flex flex-col h-screen min-w-[40%] gap-2 p-3">
+    <div className="flex flex-col h-screen min-w-[40%] gap-2 p-3 overflow-scroll">
       <h1 className="text-center text-3xl font-bold">Schedule Detail</h1>
       {!_id ? (
         <div className="text-xl italic animate-bounce text-center mt-8 ">Type a Schedule to view details</div>
       ) : (
         <div className="flex flex-col gap-2">
-          <p className="mt-4 text-xl font-bold">{title}</p>
-          <p className="text-justify">{description}</p>
+          <p className="mt-4 text-xl font-bold break-words">{title}</p>
+          <p className="text-justify break-words">{description}</p>
           <p>{time}</p>
           <p>{type}</p>
           <button id="complete" onClick={completeSchedule} className={btnStyle}>
--- src/pages/schedule/all.jsx
+++ src/pages/schedule/all.jsx
@@ -37,7 +37,7 @@ export default function All() {
       <div className="flex w-full">
         <div
           className={`flex flex-col gap-4 pb-5 ${
-            schedule.length ? "min-w-[60%]" : "min-w-full"
+            schedule.length ? "min-w-[60%] max-w-[60%]" : "min-w-full"
           } items-center px-12 max-h-[100vh] overflow-y-scroll scrollbar`}
         >
           <h1 className="text-3xl my-3 font-bold">All Schedule</h1>
@@ -53,10 +53,10 @@ export default function All() {
                   setUserSchedule(e);
                 }}
               >
-                <div className="w-full text-xl font-bold tracking-wider text-black group-hover:text-white transition-all">
+                <div className="break-words w-full text-xl font-bold tracking-wider text-black group-hover:text-white transition-all">
                   {e.title.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())}
                 </div>
-                <div className="w-full line-clamp-1">{e.description}</div>
+                <div className="w-full break-words line-clamp-2">{e.description}</div>
                 <div className="flex justify-between items-center w-full text-xs mt-1">
                   <span className={spanStyle}>{e.type}</span>
                   <span className={spanStyle + ` gap-x-2 ${e.completed && "bg-blue-100 group-hover:text-black"}`}>
