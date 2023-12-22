399ea4e461f81849062daa73b9d51475a8989df0
Aquib Alam <aquibalam399@gmail.com>
2023-12-23 01:04:29 +0530

    toploader

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,3 +1,4 @@
+import NextTopLoader from "nextjs-toploader";
 import Navbar from "./navbar";
 import { Comfortaa } from "next/font/google";
 const comfortaa = Comfortaa({ subsets: ["latin"], weight: [], display: "auto" });
@@ -5,6 +6,7 @@ const comfortaa = Comfortaa({ subsets: ["latin"], weight: [], display: "auto" })
 export default function layout({ children }) {
   return (
     <main className={`${comfortaa.className} flex`}>
+      <NextTopLoader />
       <Navbar user={"Aquib"} premium={false} />
       {children}
     </main>
